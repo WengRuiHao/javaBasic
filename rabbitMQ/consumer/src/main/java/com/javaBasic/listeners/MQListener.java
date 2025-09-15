0ae851985bb86ba28faabbd61c22641205b51c79
WengRuiHao <usually12336@gmail.com>
2025-09-15 15:49:49 +0800

    更新RabbitMQ的總結

--- rabbitMQ/consumer/src/main/java/com/javaBasic/listeners/MQListener.java
+++ rabbitMQ/consumer/src/main/java/com/javaBasic/listeners/MQListener.java
@@ -2,10 +2,7 @@ package com.javaBasic.listeners;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.ExchangeTypes;
-import org.springframework.amqp.rabbit.annotation.Exchange;
-import org.springframework.amqp.rabbit.annotation.Queue;
-import org.springframework.amqp.rabbit.annotation.QueueBinding;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.annotation.*;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
@@ -43,7 +40,7 @@ public class MQListener {
 
     @RabbitListener(bindings = @QueueBinding(
             value = @Queue(name = "direct.queue1", durable = "true"),
-            exchange = @Exchange(name = "ruihao.direct", type = ExchangeTypes.DIRECT),
+            exchange = @Exchange(name = "ruihao.direct", type = ExchangeTypes.DIRECT, arguments = @Argument(name = "x-queue-mode", value = "lazy")),
             key = {"red", "blue"}
     ))
     public void listenDirectQueue1(String msg) throws InterruptedException {
@@ -51,7 +48,7 @@ public class MQListener {
     }
 
     @RabbitListener(bindings = @QueueBinding(
-            value = @Queue(name = "direct.queue2", durable = "true"),
+            value = @Queue(name = "direct.queue2", durable = "true", arguments = @Argument(name = "x-queue-mode", value = "lazy")),
             exchange = @Exchange(name = "ruihao.direct", type = ExchangeTypes.DIRECT),
             key = {"red", "yellow"}
     ))
@@ -69,7 +66,7 @@ public class MQListener {
         System.out.println("消費者2 收到 topic.queue2 的消息: [" + msg + "] ");
     }
 
-    @RabbitListener(queues = "object.queue")
+    @RabbitListener(queues = "lazy.queue")
     public void listenObject(Map<String, Object> msg) throws InterruptedException {
         System.out.println("消費者 收到 object.queue 的消息: [" + msg + "] ");
     }
