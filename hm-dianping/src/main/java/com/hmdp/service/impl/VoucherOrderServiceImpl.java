02ff2e909836cd40c1da46a6b5e238e605e6aa22
WengRuiHao <usually12336@gmail.com>
2025-10-17 11:38:13 +0800

    優惠券秒殺下單

--- hm-dianping/src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -1,14 +1,23 @@
 package com.hmdp.service.impl;
 
+import com.hmdp.dto.Result;
+import com.hmdp.entity.SeckillVoucher;
 import com.hmdp.entity.VoucherOrder;
 import com.hmdp.mapper.VoucherOrderMapper;
+import com.hmdp.service.ISeckillVoucherService;
 import com.hmdp.service.IVoucherOrderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.RedisIsIdWorker;
+import com.hmdp.utils.UserHolder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author 虎哥
@@ -17,4 +26,53 @@ import org.springframework.stereotype.Service;
 @Service
 public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, VoucherOrder> implements IVoucherOrderService {
 
+    @Autowired
+    private ISeckillVoucherService seckillVoucherService;
+
+    @Autowired
+    private RedisIsIdWorker redisIsIdWorker;
+
+    @Override
+    @Transactional
+    public Result seckillVoucher(Long voucherId) {
+        // 1. 查詢優惠券
+        SeckillVoucher voucher = seckillVoucherService.getById(voucherId);
+        // 2. 判斷秒殺是否開始
+        if (voucher.getBeginTime().isAfter(LocalDateTime.now())) {
+            // 尚未開始
+            return Result.fail("秒殺尚未開始");
+        }
+        // 3. 判斷秒殺是否已經結束
+        if (voucher.getEndTime().isBefore(LocalDateTime.now())) {
+            // 已經結束
+            return Result.fail("秒殺已經結束");
+        }
+        // 4. 判斷庫存是否充足
+        if (voucher.getStock() < 1) {
+            // 庫存不足
+            return Result.fail("庫存不足");
+        }
+        // 5. 扣減庫存
+        boolean success = seckillVoucherService.update()
+                .setSql("stock = stock - 1")
+                .eq("voucher_id", voucherId).update();
+        if (!success) {
+            // 扣減失敗
+            return Result.fail("庫存不足");
+        }
+        // 6. 創建訂單
+        VoucherOrder voucherOrder = new VoucherOrder();
+        // 6.1 訂單 id
+        Long orderId = redisIsIdWorker.nextId("order");
+        voucherOrder.setId(orderId);
+        // 6.2 用戶 id
+        Long userId = UserHolder.getUser().getId();
+        voucherOrder.setUserId(userId);
+        // 6.3 代金券 id
+        voucherOrder.setVoucherId(voucherId);
+        save(voucherOrder);
+
+        // 7. 返回訂單id
+        return Result.ok(orderId);
+    }
 }
